4aa17745a950a3ad0a63405e41ec090d1665246c
Darron Brown <dabrown18@live.com>
2022-10-02 05:28:54 -0600

    What are actions (events)

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import {View, Text, SafeAreaView, StyleSheet, Image} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, TextInput, Button} from 'react-native';
 
 const App = () => {
-
-  const driversLicense = {
-    image: 'https://e9g2x6t2.rocketcdn.me/wp-content/uploads/2021/06/what-is-a-professional-headshot-example-header.jpg',
-    name: 'Samantha Johnson',
-    height: "5'8",
-    weight: '140 lbs',
-  };
+const [firstName, setFirstName] = useState('');
+const [lastName, setLastName] = useState('');
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.driverLicense}>
-        <Image style={styles.driverLicenseImage} source={{uri: driversLicense.image}} />
-        <Text>Name: {driversLicense.name}</Text>
-        <Text>Height: {driversLicense.height}</Text>
-        <Text>Weight: {driversLicense.weight}</Text>
-      </View>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setFirstName(text)}
+        value={firstName}
+        placeholder="Enter firstname"
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setLastName(text)}
+        value={lastName}
+        placeholder="Enter lastname"
+        keyboardType="numeric"
+      />
+      <Button
+        title={'Submit name'}
+        onPress={() => alert(`User: ${firstName} ${lastName} has been saved`)}
+      />
     </SafeAreaView>
   )
 }
@@ -25,19 +32,15 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  driverLicense: {
-    borderRadius: 5,
-    borderWidth: 2,
-    width: '100%',
-    padding: 15
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
-  driverLicenseImage: {
-    height: 80,
-    width: 80,
-    borderRadius: 80 / 2
-  }
 })
 
 export default App
