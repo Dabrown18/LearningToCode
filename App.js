54f678dce8c33651f5a9c82217e616f46f5d46b4
Darron Brown <dabrown18@live.com>
2022-10-02 05:52:01 -0600

    What are methods (functions)

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
 import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet, TextInput, Button} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, Button} from 'react-native';
 
 const App = () => {
-const [firstName, setFirstName] = useState('');
-const [lastName, setLastName] = useState('');
+  const [greeting, setGreeting] = useState('');
+
+  const getGreeting = () => {
+    const todaysDate = new Date();
+    const currentHour = todaysDate.getHours();
+
+    if (currentHour > 18) {
+      setGreeting('Good evening!');
+    } else if (currentHour > 12) {
+      setGreeting('Good afternoon!');
+    } else {
+      setGreeting('Welcome!');
+    }
+
+    return greeting;
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput
-        style={styles.input}
-        onChangeText={text => setFirstName(text)}
-        value={firstName}
-        placeholder="Enter firstname"
-        keyboardType="numeric"
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={text => setLastName(text)}
-        value={lastName}
-        placeholder="Enter lastname"
-        keyboardType="numeric"
-      />
+      <Text>{greeting}</Text>
       <Button
-        title={'Submit name'}
-        onPress={() => alert(`User: ${firstName} ${lastName} has been saved`)}
+        title={'Say Hello'}
+        onPress={getGreeting}
       />
     </SafeAreaView>
   )
